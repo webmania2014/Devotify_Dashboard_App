ea2bcd5ad2cb72d20a16d259fa9ca46f6c73c31a
webmania2014 <panda1987417@hotmail.com>
2017-04-23 08:29:45 +0200

    add addadmin component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AddadminComponent } from './component/addadmin/addadmin/addadmin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddadminComponent
   ],
   imports: [
     BrowserModule,
